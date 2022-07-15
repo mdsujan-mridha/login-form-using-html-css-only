b525852ea4d2536af872dce1439022ed444a712a
Md. Sujan <68296079+mdsujan-mridha@users.noreply.github.com>
2022-07-15 16:34:08 +0600

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("login-btn").addEventListener("click", function () {
-  let getInput = [ ];
-  const getEmail = document.getElementById("email-field").value;
-  getInput.push(getEmail);
-  const getPassword = document.getElementById("password-field").value;
-  getInput.push(getPassword);
-  console.log(getInput);
-});
